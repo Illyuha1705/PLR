165ac70755fb9df001d62e7d22a60f3822c8ea54
Illia Lotvin <ilotvin@lenovo.com>
2022-01-23 19:31:41 +0200

    1.0 [core] PLR-1 Add styles to button component

--- users-list/src/app/components/button/button.component.ts
+++ users-list/src/app/components/button/button.component.ts
@@ -1,17 +1,18 @@
-import { Component, HostBinding, Input } from '@angular/core';
-import { ButtonType }                    from '../../interfaces/button.interface';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { ButtonType }                                       from '../../interfaces/button.interface';
 
 @Component({
   selector: 'button[ul-button]',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent {
   @Input() btnType: ButtonType = 'primary';
   @Input() ariaLabel: string | undefined;
 
   @HostBinding('class') get bindClasses(): string {
-    return `ul-btn ul-btn-${this.btnType}`;
+    return `ul-btn ul-btn--${this.btnType}`;
   }
 
   @HostBinding('attr.aria-label') get bindAriaLabel(): string {
