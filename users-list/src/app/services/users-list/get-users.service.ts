f692af3c0ee8bd2ce7abfa02dcbf4689273d8f75
Illia Lotvin <ilotvin@lenovo.com>
2022-02-02 00:16:47 +0200

    1.0 [core] PLR-users Add file with constants

--- users-list/src/app/services/users-list/get-users.service.ts
+++ users-list/src/app/services/users-list/get-users.service.ts
@@ -1,27 +1,28 @@
-import { Injectable }    from '@angular/core';
-import { HttpClient }     from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { UserInterface }  from '../../interfaces/user.interface';
-import { catchError }    from 'rxjs/operators';
+import { UserInterface } from '../../interfaces/user.interface';
+import { catchError } from 'rxjs/operators';
+import { API_USERS } from '../../constants/api.constants';
 
 @Injectable()
 export class GetUsersService {
-  private url = 'https://jsonplaceholder.typicode.com/users';
-
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<UserInterface[]> {
-    return this.http.get<UserInterface[]>(this.url)
-      .pipe(
-        catchError(this.handleError<UserInterface[]>('getUsers', []))
-      );
+    return this.http
+      .get<UserInterface[]>(API_USERS)
+      .pipe(catchError(this.handleError<UserInterface[]>('getUsers', [])));
   }
 
-  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+  private handleError<T>(
+    operation = 'operation',
+    result?: T
+  ): (error: any) => Observable<T> {
     return (error: any): Observable<T> => {
       console.log(error);
 
       return of(result as T);
-    }
+    };
   }
 }
